feea492a46de4eba2055ee51ae2ebff98a8aad00
Juan Ceballos <juandavi789@hotmail.com>
2024-08-10 09:54:11 -0500

    UserController - getuser

---
+++ Controllers/UserController.js
@@ -0,0 +1,19 @@
+import { Users } from '../constants/UsersData';
+
+export const getUser = (req, res) => {
+  const { nombre, email } = req.query;
+
+  if (!nombre && !email) {
+    return res.status(400).send('Debe proporcionar un nombre o un email.');
+  }
+
+  const user = Users.find(
+    (u) => (nombre && u.nombre === nombre) || (email && u.email === email)
+  );
+
+  if (!user) {
+    return res.status(404).send('Usuario no encontrado.');
+  }
+
+  res.json(user);
+};
