ab718bea1edbbc2c36f32404889cb27a9e7b6829
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-09-07 08:52:06 -0500

    - CHG: Unnecesary folders have been deleted

--- Validators/index.js
+++
@@ -1,3 +0,0 @@
-export * from "../LogIn/Validator/LogInValidator.js";
-export * from "../Users/Validators/UsersValidator.js";
-export * from "./postsValidator.js";
--- constants/index.js
+++
@@ -1,2 +0,0 @@
-export * from "../Users/Constants/UsersData.js";
-export * from "../Posts/Constants/PostsData.js";
