c8783d99f81c5cca09b3953900d808c2ac6f72fe
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-09-28 10:06:19 -0500

    - ADD: Testing desing for UserController has been created

---
+++ Users/Tests/UserController.test.js
@@ -0,0 +1,58 @@
+describe("UserController.js", () => {
+  describe("Unfollow User", () => {
+    describe("When everything is ok", () => {
+      test("should return ok true", () => {});
+    });
+    describe("When followerUID is invalid", () => {
+      test("should return ok false and error description.", () => {});
+    });
+    describe("When followedUID is invalid", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When something went wrong.", () => {
+      test("should return ok false and error description", () => {});
+    });
+  });
+  describe("Follow User", () => {
+    describe("When everything is ok", () => {
+      test("should return ok true", () => {});
+    });
+    describe("When followerUID is invalid", () => {
+      test("should return ok false and error description.", () => {});
+    });
+    describe("When followedUID is invalid", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When something went wrong.", () => {
+      test("should return ok false and error description", () => {});
+    });
+  });
+  describe("Get Followers by UID", () => {
+    describe("When everything is ok", () => {
+      test("should return ok true", () => {});
+    });
+    describe("When something went wrong", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When UID is invalid", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When user not found", () => {
+      test("should return ok false and error description", () => {});
+    });
+  });
+  describe("Get Followed by UID", () => {
+    describe("When everything is ok", () => {
+      test("should return ok true", () => {});
+    });
+    describe("When something went wrong", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When UID is invalid", () => {
+      test("should return ok false and error description", () => {});
+    });
+    describe("When user not found", () => {
+      test("should return ok false and error description", () => {});
+    });
+  });
+});
