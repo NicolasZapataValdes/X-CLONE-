4baff0374ff3b941b593743c77a37f5ae6f0af95
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-06 17:44:19 -0500

    - ADD: LogInRouterDocs have been created to document the API with swagger

---
+++ LogIn/Docs/LogInRouterDocs.js
@@ -0,0 +1,362 @@
+export const LogInWithEmailAndPassWord = {
+  tags: ["LogIn"],
+  description: "This Endpoint allows you to logIn with email and password",
+  responses: {
+    200: {
+      description: "User loggedIn",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: true,
+              },
+              message: {
+                type: "string",
+                example: "User loggedIn succesfully",
+              },
+            },
+          },
+        },
+      },
+    },
+    400: {
+      description: "Bad Request",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Request don't pass validations.",
+              },
+              errorDescription: {
+                type: "Array",
+                example: [
+                  {
+                    type: "field",
+                    msg: "Param Email is obligatory.",
+                    path: "email",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param email is not a valid Email.",
+                    path: "email",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param PassWord is empty.",
+                    path: "passWord",
+                    location: "body",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+    500: {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Error while trying to LogIn With Email and Password",
+              },
+              errorDescription: {
+                type: "string",
+                example: "An error description",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+export const LogInUserNameAndPassWord = {
+  tags: ["LogIn"],
+  description: "This Endpoint allows you to logIn with UserName and password",
+  responses: {
+    200: {
+      description: "User loggedIn",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: true,
+              },
+              message: {
+                type: "string",
+                example: "User loggedIn succesfully",
+              },
+            },
+          },
+        },
+      },
+    },
+    400: {
+      description: "Bad Request",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Request don't pass validations.",
+              },
+              errorDescription: {
+                type: "Array",
+                example: [
+                  {
+                    type: "field",
+                    msg: "Param UserName is obligatory.",
+                    path: "UserName",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param PassWord is obligatory.",
+                    path: "passWord",
+                    location: "body",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+    500: {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example:
+                  "Error while trying to LogIn With UserName and Password",
+              },
+              errorDescription: {
+                type: "string",
+                example: "An error description",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+export const LogOutWithEmailAndPassWord = {
+  tags: ["LogIn"],
+  description: "This Endpoint allows you to logOut with email and password",
+  responses: {
+    200: {
+      description: "User loggedOut",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: true,
+              },
+              message: {
+                type: "string",
+                example: "User loggedOut succesfully",
+              },
+            },
+          },
+        },
+      },
+    },
+    400: {
+      description: "Bad Request",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Request don't pass validations.",
+              },
+              errorDescription: {
+                type: "Array",
+                example: [
+                  {
+                    type: "field",
+                    msg: "Param Email is obligatory.",
+                    path: "email",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param email is not a valid Email.",
+                    path: "email",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param PassWord is empty.",
+                    path: "passWord",
+                    location: "body",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+    500: {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Error while trying to LogOut With Email and Password",
+              },
+              errorDescription: {
+                type: "string",
+                example: "An error description",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+export const LogOutWithUserNameAndPassWord = {
+  tags: ["LogIn"],
+  description: "This Endpoint allows you to logOut with user name and password",
+  responses: {
+    200: {
+      description: "User loggedOut",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: true,
+              },
+              message: {
+                type: "string",
+                example: "User loggedOut succesfully",
+              },
+            },
+          },
+        },
+      },
+    },
+    400: {
+      description: "Bad Request",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example: "Request don't pass validations.",
+              },
+              errorDescription: {
+                type: "Array",
+                example: [
+                  {
+                    type: "field",
+                    msg: "Param UserName is obligatory.",
+                    path: "UserName",
+                    location: "body",
+                  },
+                  {
+                    type: "field",
+                    msg: "Param PassWord is empty.",
+                    path: "passWord",
+                    location: "body",
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+    500: {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              ok: {
+                type: "boolean",
+                example: false,
+              },
+              message: {
+                type: "string",
+                example:
+                  "Error while trying to LogOut With UserName and Password",
+              },
+              errorDescription: {
+                type: "string",
+                example: "An error description",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
