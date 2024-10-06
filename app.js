c0911e8a6b5bb9124f24141e551fda7ee6d0edb0
Juan Ceballos <juandavi789@hotmail.com>
2024-10-06 18:20:15 -0500

    Update swagger for post router

--- app.js
+++ app.js
@@ -1,55 +1,75 @@
-import express from "express";
-import { logInRouter } from "./LogIn/Routes/index.js";
-import { postRouter } from "./Posts/Routes/index.js";
-import { userRouter } from "./Users/Routes/index.js";
-import { createPost } from "./Posts/Docs/PostRouterDocs.js";
+import express from 'express';
+import { logInRouter } from './LogIn/Routes/index.js';
+import { postRouter } from './Posts/Routes/index.js';
+import { userRouter } from './Users/Routes/index.js';
+import {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePostContent,
+  deletePostById,
+  restorePostById,
+} from './Posts/Docs/PostRouterDocs.js';
 
-import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 export const app = express();
 
 const swaggerOptions = {
   swaggerDefinition: {
-    openapi: "3.0.1",
+    openapi: '3.0.1',
     info: {
-      title: "X-CLONE API",
-      version: "1.0.0",
-      description: "API documentation",
+      title: 'X-CLONE API',
+      version: '1.0.0',
+      description: 'API documentation',
     },
     servers: [
       {
-        url: "http://localhost:1234",
-        description: "Development server",
+        url: 'http://localhost:1234',
+        description: 'Development server',
       },
     ],
     tags: [
       {
-        name: "Posts",
+        name: 'Posts',
       },
       {
-        name: "Users",
+        name: 'Users',
       },
       {
-        name: "LogIn",
+        name: 'LogIn',
       },
     ],
     paths: {
-      "/api/v1/posts/": {
+      '/api/v1/posts/all': {
+        get: getAllPosts,
+      },
+      '/api/v1/posts/:id': {
+        get: getPostById,
+        patch: updatePostContent,
+      },
+      '/api/v1/posts/': {
         post: createPost,
+      },
+      '/api/v1/posts/delete/:id': {
+        patch: deletePostById,
+      },
+      '/api/v1/posts/restore/:id': {
+        patch: restorePostById,
       },
     },
   },
   apis: [
-    "./LogIn/Routes/LogInRouter.js",
-    "./Posts/Routes/postRouter.js",
-    "./Users/Routes/UsersRouter.js",
+    './LogIn/Routes/LogInRouter.js',
+    './Posts/Routes/postRouter.js',
+    './Users/Routes/UsersRouter.js',
   ],
 };
 
 app.use(express.json());
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/api/v1", userRouter);
-app.use("/api/v1", logInRouter);
-app.use("/api/v1", postRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api/v1', userRouter);
+app.use('/api/v1', logInRouter);
+app.use('/api/v1', postRouter);
