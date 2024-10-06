266831998e0aa3bf6e0700932716f164ad4a095e
Juan Ceballos <juandavi789@hotmail.com>
2024-10-06 15:47:42 -0500

    Update routes for posts

--- Posts/Routes/postRouter.js
+++ Posts/Routes/postRouter.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 export const postRouter = express.Router();
 
 import {
@@ -8,23 +8,23 @@ import {
   updatePostContent,
   deletePostById,
   restorePostById,
-} from "../Controllers/index.js";
+} from '../Controllers/index.js';
 
 import {
   validatePost,
   validateUpdatePostContent,
   validateDeletePostById,
   validateRestorePostById,
-} from "../Validators/index.js";
+  validateGetPostById,
+} from '../Validators/index.js';
 
-postRouter.get("/posts", getAllPosts);
-// postRouter.get('/posts/:id', validateGetPostById(), getPostById);
-postRouter.get("/posts/:id", getPostById);
-postRouter.post("/posts", validatePost(), createPost);
-postRouter.patch("/posts/:id", validateUpdatePostContent(), updatePostContent);
-postRouter.patch("/posts/delete/:id", validateDeletePostById(), deletePostById);
+postRouter.get('/posts/all', getAllPosts);
+postRouter.get('/posts/:id', validateGetPostById(), getPostById);
+postRouter.post('/posts', validatePost(), createPost);
+postRouter.patch('/posts/:id', validateUpdatePostContent(), updatePostContent);
+postRouter.patch('/posts/delete/:id', validateDeletePostById(), deletePostById);
 postRouter.patch(
-  "/posts/restore/:id",
+  '/posts/restore/:id',
   validateRestorePostById(),
   restorePostById
 );
