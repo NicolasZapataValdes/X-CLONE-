9e080e34e0b3436f333660e69e94e2049c5216db
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-15 23:14:02 -0500

    - ADD: Function To Encrypt and Decrypt JWT have been added
    - CHG: Functions to Generate and Validate token have been updated to work with Crypto functions

--- Utils/Functions/Functions.js
+++ Utils/Functions/Functions.js
@@ -1,9 +1,10 @@
-import { verify, sign } from "jsonwebtoken";
+import jsonwebtoken from "jsonwebtoken";
 import dotenv from "dotenv";
+import crypto from "node:crypto";
 dotenv.config();
 
-export function GenerateAccessToken(UserName) {
-  return sign(UserName, process.env.JWSSecretKey, { expiresIn: "1800s" });
+export function GenerateAccessToken(PayLoad) {
+  return EncryptJWTToken(jsonwebtoken.sign(PayLoad, process.env.JWTSecretKey));
 }
 
 export function ValidateToken(req, res, next) {
@@ -12,28 +13,61 @@ export function ValidateToken(req, res, next) {
     const token = authHeader && authHeader.split(" ")[1];
 
     if (!token)
-      return res.sendStatus(401).json({
+      return res.status(401).json({
         ok: false,
         message: "Access Token in headers is empty.",
       });
 
-    verify(token, process.env.JWSSecretKey, (error, decodedUserName) => {
-      if (error)
-        return res.sendStatus(403).json({
-          ok: false,
-          message: "Access Token cannot be decoded.",
-        });
+    jsonwebtoken.verify(
+      DecryptJWTToken(token),
+      process.env.JWTSecretKey,
+      (error, decodedUserName) => {
+        if (error)
+          return res.status(403).json({
+            ok: false,
+            message: "Access Token cannot be decoded.",
+          });
 
-      req.user = decodedUserName;
-      next();
-    });
+        req.user = decodedUserName;
+        next();
+      }
+    );
   } catch (error) {
-    response.status(500).json({
+    res.status(500).json({
       ok: false,
       message: "Error while trying verify AccessToken",
       errorDescription: error?.message,
     });
   }
+}
+
+function EncryptJWTToken(Token) {
+  const InitialVector = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    Buffer.from(process.env.CryptoSecretKey),
+    InitialVector
+  );
+
+  let encrypted = cipher.update(Token, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return InitialVector.toString("hex") + ":" + encrypted;
+}
+
+function DecryptJWTToken(EncryptedToken) {
+  const parts = EncryptedToken.split(":");
+  const InitialVector = Buffer.from(parts.shift(), "hex");
+  const EncryptedJWT = Buffer.from(parts.join(":"), "hex");
+
+  const decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    Buffer.from(process.env.CryptoSecretKey),
+    InitialVector
+  );
+  let decrypted = decipher.update(EncryptedJWT, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  return decrypted;
 }
 
 export function getParsedCurrentDateTime() {
