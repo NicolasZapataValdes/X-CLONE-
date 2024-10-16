1cbd7ad56287a05fd8ad855e4aafbfa2b7aaf446
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-15 23:32:52 -0500

    - CHG: Functions to LogIn have been updated to send AcessToken when everything is ok

--- LogIn/Controller/LogInController.js
+++ LogIn/Controller/LogInController.js
@@ -1,5 +1,6 @@
 import { UserModel } from "../../Users/Models/index.js";
 import { validationResult } from "express-validator";
+import { GenerateAccessToken } from "../../Utils/Functions/index.js";
 
 export async function LogInWithEmailAndPassWord(request, response) {
   try {
@@ -22,9 +23,18 @@ export async function LogInWithEmailAndPassWord(request, response) {
 
     if (queryResult.matchedCount === 0) throw new Error("User not found.");
 
+    const UserUID = await UserModel.findOne(
+      { email: email, passWord: passWord },
+      "_id"
+    );
+
+    if (!UserUID)
+      throw new Error("An error ocurred while trying to get UserUID.");
+
     response.json({
       ok: true,
       message: "User loggedIn succesfully",
+      AccessToken: GenerateAccessToken(UserUID._id.toString()),
     });
   } catch (error) {
     response.status(500).json({
@@ -57,9 +67,18 @@ export async function LogInWithUserNameAndPassWord(request, response) {
 
     if (queryResult.matchedCount === 0) throw new Error("User not found.");
 
+    const UserUID = await UserModel.findOne(
+      { userName: UserName, passWord: passWord },
+      "_id"
+    );
+
+    if (!UserUID)
+      throw new Error("An error ocurred while trying to get UserUID.");
+
     response.json({
       ok: true,
       message: "User logged In succesfully",
+      AccessToken: GenerateAccessToken(UserUID._id.toString()),
     });
   } catch (error) {
     response.status(500).json({
