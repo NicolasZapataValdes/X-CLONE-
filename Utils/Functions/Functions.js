09c206e22ee439e3a54cf5761d989ed8cdf2a399
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-15 23:47:31 -0500

    - CHG: Function to get CurrentDateTime has been updated and improved

--- Utils/Functions/Functions.js
+++ Utils/Functions/Functions.js
@@ -3,6 +3,35 @@ import dotenv from "dotenv";
 import crypto from "node:crypto";
 dotenv.config();
 
+function EncryptJWTToken(Token) {
+  const InitialVector = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    Buffer.from(process.env.CryptoSecretKey),
+    InitialVector
+  );
+
+  let encrypted = cipher.update(Token, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return InitialVector.toString("hex") + ":" + encrypted;
+}
+
+function DecryptJWTToken(EncryptedToken) {
+  const parts = EncryptedToken.split(":");
+  const InitialVector = Buffer.from(parts.shift(), "hex");
+  const EncryptedJWT = Buffer.from(parts.join(":"), "hex");
+
+  const decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    Buffer.from(process.env.CryptoSecretKey),
+    InitialVector
+  );
+  let decrypted = decipher.update(EncryptedJWT, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  return decrypted;
+}
+
 export function GenerateAccessToken(PayLoad) {
   return EncryptJWTToken(jsonwebtoken.sign(PayLoad, process.env.JWTSecretKey));
 }
@@ -41,46 +70,8 @@ export function ValidateToken(req, res, next) {
   }
 }
 
-function EncryptJWTToken(Token) {
-  const InitialVector = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(
-    "aes-256-cbc",
-    Buffer.from(process.env.CryptoSecretKey),
-    InitialVector
-  );
-
-  let encrypted = cipher.update(Token, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return InitialVector.toString("hex") + ":" + encrypted;
-}
-
-function DecryptJWTToken(EncryptedToken) {
-  const parts = EncryptedToken.split(":");
-  const InitialVector = Buffer.from(parts.shift(), "hex");
-  const EncryptedJWT = Buffer.from(parts.join(":"), "hex");
-
-  const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    Buffer.from(process.env.CryptoSecretKey),
-    InitialVector
-  );
-  let decrypted = decipher.update(EncryptedJWT, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-
-  return decrypted;
-}
-
 export function getParsedCurrentDateTime() {
   const currentTime = new Date();
-
-  const day = currentTime.getDate().toString().padStart(2, "0");
-  const moth = (currentTime.getMonth() + 1).toString().padStart(2, "0"); // +1 porque los meses empiezan en 0
-  const year = currentTime.getFullYear();
-  const hours = currentTime.getHours().toString().padStart(2, "0");
-  const minutes = currentTime.getMinutes().toString().padStart(2, "0");
-  const seconds = currentTime.getSeconds().toString().padStart(2, "0");
-
-  const parsedDateTime = `${day}/${moth}/${year} ${hours}:${minutes}:${seconds}`;
-
-  return parsedDateTime;
+  currentTime.setHours(currentTime.getHours() - 5);
+  return currentTime.toISOString();
 }
