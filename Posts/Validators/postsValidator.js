cc4bf639ff2ea19b2e05f02cdc67fb48be29c52f
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-15 23:47:50 -0500

    - CHG: Function to create a Post has been updated to work with request.user

--- Posts/Validators/postsValidator.js
+++ Posts/Validators/postsValidator.js
@@ -1,53 +1,46 @@
-import { body, check, param } from 'express-validator';
-import { ValidateToken } from '../../Utils/Functions/index.js';
+import { body, check, param } from "express-validator";
+import { ValidateToken } from "../../Utils/Functions/index.js";
 
 export const validatePost = () => {
   return [
-    check('createdAt')
-      .isDate({ format: 'DD-MM-YYYY' })
-      .withMessage('Param createdAt is not a valid Date'),
-    body('creatorUID').notEmpty().withMessage('Param creatorUID is empty'),
-    check('updatedAt')
-      .isDate({ format: 'DD-MM-YYYY' })
-      .withMessage('Param createdAt is not a valid Date'),
-    body('deleted').isBoolean().withMessage('Param deleted is not a Boolean'),
-    body('content').notEmpty().withMessage('Param content is empty'),
+    ValidateToken,
+    body("content").notEmpty().withMessage("Param content is empty"),
   ];
 };
 
 export const validateGetPostById = () => {
   return [
-    param('id')
+    param("id")
       .isLength({ min: 20 })
-      .withMessage('Param id does not have minimal length of 20'),
-    param('id').notEmpty().withMessage('Param id is empty'),
+      .withMessage("Param id does not have minimal length of 20"),
+    param("id").notEmpty().withMessage("Param id is empty"),
     ValidateToken,
   ];
 };
 
 export const validateUpdatePostContent = () => {
   return [
-    body('content').notEmpty().withMessage('Param content is empty'),
+    body("content").notEmpty().withMessage("Param content is empty"),
     ValidateToken,
   ];
 };
 
 export const validateDeletePostById = () => {
   return [
-    param('id')
+    param("id")
       .isLength({ min: 20 })
-      .withMessage('Param id does not have minimal length of 20'),
-    param('id').notEmpty().withMessage('Param id is empty'),
+      .withMessage("Param id does not have minimal length of 20"),
+    param("id").notEmpty().withMessage("Param id is empty"),
     ValidateToken,
   ];
 };
 
 export const validateRestorePostById = () => {
   return [
-    param('id')
+    param("id")
       .isLength({ min: 20 })
-      .withMessage('Param id does not have minimal length of 20'),
-    param('id').notEmpty().withMessage('Param id is empty'),
+      .withMessage("Param id does not have minimal length of 20"),
+    param("id").notEmpty().withMessage("Param id is empty"),
     ValidateToken,
   ];
 };
