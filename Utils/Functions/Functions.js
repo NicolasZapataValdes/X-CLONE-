45f48ec8a1b9d2205b5d5511432a28f8ade83434
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-15 22:30:19 -0500

    - ADD: Functions to Generate and Validate Access Token have been created

--- Utils/Functions/Functions.js
+++ Utils/Functions/Functions.js
@@ -1,3 +1,41 @@
+import { verify, sign } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export function GenerateAccessToken(UserName) {
+  return sign(UserName, process.env.JWSSecretKey, { expiresIn: "1800s" });
+}
+
+export function ValidateToken(req, res, next) {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token)
+      return res.sendStatus(401).json({
+        ok: false,
+        message: "Access Token in headers is empty.",
+      });
+
+    verify(token, process.env.JWSSecretKey, (error, decodedUserName) => {
+      if (error)
+        return res.sendStatus(403).json({
+          ok: false,
+          message: "Access Token cannot be decoded.",
+        });
+
+      req.user = decodedUserName;
+      next();
+    });
+  } catch (error) {
+    response.status(500).json({
+      ok: false,
+      message: "Error while trying verify AccessToken",
+      errorDescription: error?.message,
+    });
+  }
+}
+
 export function getParsedCurrentDateTime() {
   const currentTime = new Date();
 
