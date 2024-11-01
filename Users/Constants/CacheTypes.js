b2597b2f2affcf930df60c1f95e99680be6be125
Juan Ceballos <juandavi789@hotmail.com>
2024-10-31 23:48:54 -0500

    Remove unnecesary files

--- Users/Constants/CacheTypes.js
+++
@@ -1,8 +0,0 @@
-export const Types = {
-  GetFollowedUsersIDByUID: "[User] Followed_IDS",
-  GetFollowersByUid: "[User] Followers",
-  GetFollowedUsersByUID: "[User] Followed",
-  GetUserByUserName: "[User] UserName",
-  GetUserByEmail: "[User] Email",
-  GetUserByUID: "[User] UID",
-};
